b83415898e34ce98c514a1b0a2bd5cf05b65b4f4
Pablo Salas <pascifuentes@gmail.com>
2022-05-23 21:13:35 -0400

    creacion base dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import LoginScreen from './components/login/LoginScreen';
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import ClienteScreen from './components/cliente/ClienteScreen';
 import VehiculoScreen from './components/vehiculo/VehiculoScreen';
+import Layout from './components/Layout';
+import ResponsiveDrawer from './components/ui/ResponsiveDrawer';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
         <Route path="/login" element={<LoginScreen />} />
-        <Route path="/cliente" element={<ClienteScreen />} />
-        <Route path="/vehiculo" element={<VehiculoScreen />} />
+
+        <Route path="/" element={<ResponsiveDrawer />} >
+          <Route path="/cliente" element={<ClienteScreen />} />
+          <Route path="/vehiculo" element={<VehiculoScreen />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
