a37367f4dad06d64c85a658c20367e3fc50a0d40
Pablo Salas <pascifuentes@gmail.com>
2022-05-29 14:52:26 -0400

    Implemento login

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import VehiculoScreen from './components/vehiculo/VehiculoScreen';
 import Layout from './components/Layout';
 import { useState } from 'react';
 import UsuarioScreen from './components/usuario/UsuarioScreen';
+import { AuthProvider } from './context/AuthContex';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/" element={<Layout />} >
-          <Route path="/cliente" element={<ClienteScreen/>} />
-          <Route path="/vehiculo" element={<VehiculoScreen  />} />
-          <Route path="/usuarios" element={<UsuarioScreen />} />
-          
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/" element={<Layout />} >
+            <Route path="/cliente" element={<ClienteScreen />} />
+            <Route path="/vehiculo" element={<VehiculoScreen />} />
+            <Route path="/usuarios" element={<UsuarioScreen />} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
 import Button from '@mui/material/Button'
 import NavBar from './ui/NavBar'
 import { Box, Grid } from '@mui/material'
 import SideBar from './ui/SideBar'
+import AuthContext from '../context/AuthContex'
+
 const Layout = () => {
-    return (
-        <Grid container>
-            <Grid md={3}>
-                <SideBar style={{
-                    position: 'sticky',
-                    top: '0',
-                    zIndex: '1',
-                    backgroundColor: '#010101',
-                    height: '100vh',
-                    color: '#fff'
-                }} />
-            </Grid>
-            <Grid md={9}>
 
-                <NavBar />
-                <Box component="main" p={2}>
+    const { auth } = useContext(AuthContext);
+    if (auth.logged == false) {
+        return <Navigate to="/login" replace={true} />
+    } else {
+        return (
+            <Grid container>
+                <Grid md={3}>
+                    <SideBar style={{
+                        position: 'sticky',
+                        top: '0',
+                        zIndex: '1',
+                        backgroundColor: '#010101',
+                        height: '100vh',
+                        color: '#fff'
+                    }} />
+                </Grid>
+                <Grid md={9}>
 
-                    <Outlet />
-                </Box >
+                    <NavBar />
+                    <Box component="main" p={2}>
+
+                        <Outlet />
+                    </Box >
+                </Grid>
             </Grid>
-        </Grid>
-    )
+        )
+    }
 }
-
 export default Layout
