0fc280e7f63bfa6ccb3a6182fb5c4c82b5aeff3a
rashord <richard.calbullanca.v@gmail.com>
2022-06-02 19:57:43 -0400

    Funcion basica agregar

--- src/components/usuario/ListaUsuarios.jsx
+++ src/components/usuario/ListaUsuarios.jsx
@@ -1,24 +1,69 @@
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, IconButton, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide, FormControl, FormLabel, FormGroup, FormHelperText, TextField, Grid, Divider, MenuItem, InputLabel, Select } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component'
 
-const columns = [
-    {
-        name: 'Nombre',
-        selector: row => row.nombreU,
-    },
-    {
-        name: 'Email',
-        selector: row => row.email,
-    },
-    {
-        name: 'Rol',
-        selector: row => row.cRolU,
-    },
-];
-
 const ListaUsuarios = () => {
+    
+	const handleButtonClick = () => {
+		console.log('clicked');
+	};
+
+    const columns = [
+    
+        {
+            name: 'Nombre',
+            selector: row => row.nombreU,
+            grow : 2,
+        },
+        {
+            name: 'Email',
+            selector: row => row.email,
+            grow : 2,
+        },
+        {
+            name: 'Rol',
+            selector: row => row.cRolU,
+            grow : 2,
+        },
+        {	
+            cell: () => 
+            <Button 
+                onClick={handleButtonClick} 
+                variant = "outlined"
+                size = "small"
+                startIcon={<EditIcon fontSize = "small" />}
+            >
+                Modificar
+            </Button>,
+            ignoreRowClick: true,
+            allowOverflow: true,
+            button: true,
+            left: true
+            
+        },
+        {			
+            cell: () => 
+            <Button 
+                onClick={handleButtonClick} 
+                variant = "outlined"
+                color = 'error'
+                size = "small"
+                startIcon={<DeleteIcon fontSize = "small"/>}
+            >
+                Eliminar 
+            </Button>,
+            ignoreRowClick: true,
+            allowOverflow: true,
+            button: true,
+            right: true,
+
+        },
+    ];
+
+    
 
     const [usuarios,setUsuarios] = useState([]);
 
@@ -34,7 +79,6 @@ const ListaUsuarios = () => {
     
     return (
         <DataTable
-            title ='Lista de usuarios'
             columns ={columns}
             data = {usuarios}
         />
--- src/components/usuario/UsuarioScreen.jsx
+++ src/components/usuario/UsuarioScreen.jsx
@@ -1,12 +1,18 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import ListaUsuarios from './ListaUsuarios'
+import AgregarUsuarios from './AgregarUsuarios'
 
 const UsuarioScreen = () => {
   return (
     <div>
       <Grid container>
-        <Typography variant="h4" align = 'center'>Usuarios</Typography>
+        <Grid xs={12}>
+        <Typography variant="h4">Usuarios</Typography>
+        </Grid>
+        <Grid xs={12} align = 'right'>
+          <AgregarUsuarios/>
+        </Grid>
         <Grid xs={12}>
           <ListaUsuarios />
         </ Grid>
