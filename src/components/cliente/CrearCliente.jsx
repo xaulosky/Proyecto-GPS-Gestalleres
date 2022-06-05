ddd8ba651db3ac3610733b7198f4cd77dcf370a6
Pablo Salas <51441267+xaulosky@users.noreply.github.com>
2022-06-05 02:02:57 -0400

    Agregamos cliente al master

---
+++ src/components/cliente/CrearCliente.jsx
@@ -0,0 +1,134 @@
+import {
+    Select,
+    FormControl,
+    MenuItem,
+    Input,
+    InputLabel,
+    Stack,
+    TextField,
+    Container,
+    Button,
+    Box
+  } from "@mui/material";
+  import axios from "axios";
+  import React, { useEffect, useState } from "react";
+  
+  const CrearCliente = ({getClientes}) => {
+      const [form, setForm] = useState({
+          rutC: "",
+          emailC: "",
+          nombreC: "",
+          apellidoC: "",
+          direccionC: "",
+          estadoC: "",
+          cComuna: "",
+      });
+      const onChange = (e) => {
+          setForm({
+              ...form,
+              [e.target.name]: e.target.value,
+          });
+      };
+      const onSubmit = (e) => {
+          e.preventDefault();
+          console.log(form);
+          axios
+              .post("http://localhost:8080/apigps/api/cliente.php", form)
+              .then((res) => {
+                setForm({
+                  rutC: "",
+                  emailC: "",
+                  nombreC: "",
+                  apellidoC: "",
+                  direccionC: "",
+                  estadoC: "",
+                  cComuna: "",
+              });
+                  console.log(res);
+                  getClientes();
+                  
+              })
+              .catch((err) => {
+                  console.log(err);
+              });
+      };
+      
+      const [comunas, setComunas] = useState([]);
+      const getComunas = async () => {
+          await axios.get("http://localhost:8080/apigps/api/comuna.php")
+              .then((res) => {
+                  setComunas(res.data);
+                  console.log(res.data);
+              })
+              .catch((err) => {
+                  console.log(err);
+              });
+      };
+      useEffect(() => {
+          getComunas();
+      }, []);
+      
+    return (
+      <>
+        <Container maxWidth="lg">
+          <Box component="form" onSubmit={onSubmit}>
+            <Stack spacing={2} direction={"row"}>
+              <FormControl fullWidth>
+                <TextField id="rutC" value={form.rutC} label="Rut" name="rutC" onChange={onChange}/>
+              </FormControl>
+              <FormControl fullWidth>
+                <TextField id="emailc" value={form.emailC} label="Email" name="emailC" onChange={onChange}/>
+              </FormControl>
+              <FormControl fullWidth>
+                <TextField id="nombreC" value={form.nombreC} label="Nombre" name="nombreC" onChange={onChange}/>
+              </FormControl>
+              <FormControl fullWidth>
+                <TextField id="apellidoC" value={form.apellidoC} label="Apellido" name="apellidoC" onChange={onChange}/>
+              </FormControl>
+              <FormControl fullWidth>
+                <TextField id="direccionC" value={form.direccionC} label="Direccion" name="direccionC" onChange={onChange}/>
+              </FormControl>
+              <FormControl fullWidth>
+                {/* select */}
+                <InputLabel id="demo-simple-select-label">Estado</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  name="estadoC"
+                  value={form.estadoC}
+                  defaultValue={form.estadoC}
+                  onChange={onChange}
+                >
+                  <MenuItem value={1}>Activo</MenuItem>
+                  <MenuItem value={2}>Inactivo</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                {/* select */}
+                <InputLabel id="demo-simple-select-label">Comuna</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  name="cComuna"
+                  value={form.cComuna}
+                  defaultValue={form.cComuna}
+                  onChange={onChange}
+                >
+                  {comunas.map((comunas) => (
+                    <MenuItem key={comunas.cComuna} value={comunas.cComuna}>{comunas.cComuna}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <Button variant="contained" color="primary" type="submit">
+                  Crear Cliente
+                </Button>
+              </FormControl>
+            </Stack>
+          </Box>
+        </Container>
+      </>
+    );
+  };
+  
+  export default CrearCliente;
---
+++ src/components/cliente/TablaClientes.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import DataTable from 'react-data-table-component'
+import { Filter } from '@mui/icons-material';
+import { TextField } from '@mui/material';
+import CrearCliente from './CrearCliente';
+
+const paginationComponentOptions = {
+    rowsPerPageText: 'Filas por página',
+    rangeSeparatorText: 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todos',
+};
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+    <div>
+        <TextField
+            id="search"
+            type="text"
+            placeholder="Filter by name"
+            aria-label="Search input"
+            value={filterText}
+            onChange={onFilter}
+        />
+        <button onClick={onClear}>Clear</button>
+    </div>
+);
+
+const TablaClientes = () => {
+
+    const columns = [
+        {
+            name: 'Id',
+            selector: row => row.cCliente,
+            sortable: true,
+            width: '10%',
+            right: true,
+            center: true,
+
+        },
+        {
+            name: 'Rut',
+            selector: row => row.rutC,
+            sortable: true
+        },
+        {
+            name: 'Email',
+            selector: row => row.emailC,
+            sortable: true
+        }, {
+            name: 'Nombre',
+            selector: row => row.nombreC,
+            sortable: true
+        }, {
+            name: 'Apellido',
+            selector: row => row.apellidoC,
+            sortable: true
+        }, {
+            name: 'Dirección',
+            selector: row => row.direccionC,
+            sortable: true
+        }, {
+            name: 'Estado',
+            selector: row => row.estadoC,
+            sortable: true
+        },
+        {
+            name: 'Comuna',
+            selector: row => row.cComuna,
+            sortable: true
+        },
+    ];
+
+    const [clientes, setClientes] = useState([])
+
+    const getClientes = async () => {
+        await axios.get(import.meta.env.VITE_APP_BACKEND_URL + 'cliente.php')
+            .then(res => {
+                setClientes(res.data)
+            })
+    }
+
+    const [filterText, setFilterText] = useState('');
+    const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+    const filteredItems = clientes.filter(
+        item => item.nombreC && item.nombreC.toLowerCase().includes(filterText.toLowerCase())
+    );
+    const subHeaderComponentMemo = React.useMemo(() => {
+        const handleClear = () => {
+            if (filterText) {
+                setResetPaginationToggle(!resetPaginationToggle);
+                setFilterText('');
+            }
+        };
+        return (
+            <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
+        );
+    }, [filterText, resetPaginationToggle]);
+
+    useEffect(() => {
+        getClientes()
+    }, [])
+
+    return (
+        <>
+            <DataTable
+                title="Lista de clientes"
+                columns={columns}
+                data={filteredItems}
+                direction="auto"
+                fixedHeader
+                fixedHeaderScrollHeight="300px"
+                highlightOnHover
+                noContextMenu
+                pagination
+                persistTableHead
+                pointerOnHover
+                responsive
+                subHeader
+                paginationComponentOptions={paginationComponentOptions}
+                paginationResetDefaultPage={resetPaginationToggle}
+                subHeaderComponent={subHeaderComponentMemo}
+            />
+            <CrearCliente getClientes={getClientes} />
+        </>
+    )
+}
+
+export default TablaClientes
