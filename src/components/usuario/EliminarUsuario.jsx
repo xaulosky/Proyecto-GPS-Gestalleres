7d9296a900cc6c61b98274e4111d781745bd51c9
Anibal <anibal.gatica1701@gmail.com>
2022-06-16 16:35:17 -0400

    dgfhgfgyf

--- src/components/usuario/EliminarUsuario.jsx
+++ src/components/usuario/EliminarUsuario.jsx
@@ -10,7 +10,6 @@ import {
   MenuItem, InputLabel, Select, Modal, Box, makeStyles, Typography
 } from '@mui/material'
 
-<<<<<<< HEAD
 const EliminarUsuario = ({ row }) => {
 
   function prueba(row) {
@@ -89,84 +88,6 @@ const EliminarUsuario = ({ row }) => {
       </Dialog>
     </div>
   );
-=======
-const EliminarUsuario = ({row}) => {
-    
-    function prueba(row)  {
-      
-        eliminar(row)
-        handleClose()
-    }
-  
-    const [open, setOpen]   = useState(false);
-  
-    const handleOpen = () => {
-      setOpen(true);
-    };
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    const eliminar = (row) =>{
-        console.log(row.cUsuario)
-        axios.delete(import.meta.env.VITE_APP_BACKEND_URL+'usuario.php?id='+row.cUsuario)
-        .then(respuesta =>{
-            console.log(respuesta)
-        })
-   
-    }
- 
-    return (
-      <div>
-          
-          <Button
-                   sx={{
-                      '& > :not(style)': {
-                        py: 1.5,
-                        
-                      },
-                    }}
-                  color = 'error'
-                  onClick={handleOpen}
-                  endIcon={<DeleteIcon />}
-                  title = 'Eliminar Usuario'
-              />
-          
-          
-                  
-          <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-          > 
-              <DialogTitle id="alert-dialog-title">{
-              "Eliminar usuario "
-              }</DialogTitle>
-  
-              <DialogContent>
-                  <DialogContentText id="alert-dialog-description">
-                  <p>
-                    ¿Esta seguro de eliminar a {' '}
-                        <span style={{color: 'black'}}>{row.nombreU} </span>{' '}
-                    ?
-                     </p>
-                      
-                  </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                  <Button onClick={()=>prueba(row)} autoFocus >
-                      Aceptar
-                  </Button>
-                  <Button onClick={handleClose}>
-                      Cancelar
-                  </Button>
-                  
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
 }
 
 export default EliminarUsuario
--- src/components/usuario/ListaUsuarios.jsx
+++ src/components/usuario/ListaUsuarios.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     Table, TableBody, TableCell,
     TableContainer, TableHead,
@@ -10,22 +9,11 @@ import {
     MenuItem, InputLabel, Select, Modal, Box, makeStyles, Stack
 } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
-=======
-import { Table, TableBody, TableCell, 
-    TableContainer, TableHead, 
-    TableRow, Button, IconButton, 
-    Dialog, DialogActions, DialogContent, 
-    DialogContentText, DialogTitle, Slide, 
-    FormControl, FormLabel, FormGroup, 
-    FormHelperText, TextField, Grid, Divider, 
-    MenuItem, InputLabel, Select, Modal, Box, makeStyles, Stack } from '@mui/material'
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component'
 import AgregarUsuarios from './AgregarUsuarios'
 import EliminarUsuario from './EliminarUsuario';
-<<<<<<< HEAD
 
 function btnEditar(row) {
     return (
@@ -50,9 +38,6 @@ function btnEditar(row) {
 
     )
 }
-=======
-import EditarUsuario from './EditarUsuario'
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
 
 function rol(row) {
     if (row.cRolU == 1) {
@@ -86,25 +71,16 @@ const ListaUsuarios = () => {
         {
             name: 'Nombre',
             selector: row => row.nombreU,
-<<<<<<< HEAD
             grow: 1,
-=======
-            grow : 1.3,
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
         },
         {
             name: 'Email',
             selector: row => row.email,
-<<<<<<< HEAD
             grow: 1,
-=======
-            grow : 1.3,
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
         },
         {
             name: 'Rol',
             cell: (row) =>
-<<<<<<< HEAD
                 rol(row),
 
         },
@@ -114,31 +90,11 @@ const ListaUsuarios = () => {
                 <Stack direction={"row"}>
                     <EliminarUsuario row={row} />
                     {btnEditar(row)}
-=======
-            rol(row),
-            grow : 0.8,
-           
-        },
-        
-        {	
-            name: 'Acciones',		
-            cell: (row) => (
-                <Stack direction="row" spacing={1} justifyContent = 'flex-start' >
-                    <EditarUsuario row={row}/>
-                    <EliminarUsuario row={row}/>
-                    <p></p>
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
                 </Stack>
             ),
             ignoreRowClick: true,
             allowOverflow: true,
             button: true,
-<<<<<<< HEAD
-
-=======
-            grow : 2,
-            
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
         },
     ];
 
@@ -157,7 +113,6 @@ const ListaUsuarios = () => {
     return (
 
         <DataTable
-<<<<<<< HEAD
             title='Lista de usuarios'
             columns={columns}
             data={usuarios}
@@ -173,23 +128,6 @@ const ListaUsuarios = () => {
             subHeader
             subHeaderComponent={<AgregarUsuarios />}
         />
-=======
-        title='Lista de usuarios'
-        columns ={columns}
-        data = {usuarios}
-        direction="auto"
-        fixedHeader
-        fixedHeaderScrollHeight="600px"
-        highlightOnHover
-        noContextMenu
-        pagination
-        persistTableHead
-        pointerOnHover
-        responsive
-        subHeader      
-        subHeaderComponent={<AgregarUsuarios/>} 
-    />  
->>>>>>> 6b599c3ae6d29155aae8ed5bbd7f7b326575ac06
     )
 }
 
